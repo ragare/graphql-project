1043055a59afc0339bf74ff538544a670efc19fb
Rafael Garcia <dev.ragare@gmail.com>
2020-07-31 07:25:46 +0200

    casa

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,19 +3,25 @@ const _ = require('lodash');
 
 
 var usersData = [
-    {id:'1', name: 'Bond', age: 36, profession: 'Consultant'},
-    {id:'13', name: 'Anna', age: 26, profession: 'Professional killer'},
-    {id:'211', name: 'Bella', age: 16, profession: 'Rugby player'},
-    {id:'19', name: 'Gina', age: 26, profession: 'Doctor'},
-    {id:'150', name: 'Georgina', age: 36, profession: 'Baker'}
+    { id: '1', name: 'Bond', age: 36, profession: 'Consultant' },
+    { id: '13', name: 'Anna', age: 26, profession: 'Professional killer' },
+    { id: '211', name: 'Bella', age: 16, profession: 'Rugby player' },
+    { id: '19', name: 'Gina', age: 26, profession: 'Doctor' },
+    { id: '150', name: 'Georgina', age: 36, profession: 'Baker' }
 ];
 
 var hobbiesData = [
-    {id:'1', title:'Programming', description:'Desc programming'},
-    {id:'2', title:'Rowing', description:'Desc rowing'},
-    {id:'3', title:'Swimming', description:'Desc swimming'},
-    {id:'4', title:'Fencing', description:'Desc fencing'},
-    {id:'5', title:'Haking', description:'Desc haking'}
+    { id: '1', title: 'Programming', description: 'Desc programming' },
+    { id: '2', title: 'Rowing', description: 'Desc rowing' },
+    { id: '3', title: 'Swimming', description: 'Desc swimming' },
+    { id: '4', title: 'Fencing', description: 'Desc fencing' },
+    { id: '5', title: 'Haking', description: 'Desc haking' }
+];
+
+var postData = [
+    { id: '1', comment: 'Very nice' },
+    { id: '2', comment: 'Amazing thing' },
+    { id: '3', comment: 'I cant belive it' }
 ]
 
 
@@ -33,10 +39,10 @@ const UserType = new GraphQLObjectType({
     name: 'User',
     description: 'Documentation for user...',
     fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        age: {type: GraphQLInt},
-        profession: {type: GraphQLString}
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        profession: { type: GraphQLString }
     })
 });
 
@@ -44,11 +50,20 @@ const HobbyType = new GraphQLObjectType({
     name: 'Hobby',
     description: 'Documentation for hobby',
     fields: () => ({
-        id: {type: GraphQLID},
-        title: {type: GraphQLString},
-        description: {type: GraphQLString}
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        description: { type: GraphQLString }
     })
-})
+});
+
+const PostType = new GraphQLObjectType({
+    name: 'Post',
+    description: 'Documentation for post...',
+    fields: () => ({
+        id: { type: GraphQLID },
+        comment: { type: GraphQLString }
+    })
+});
 
 
 // RootQuery
@@ -59,19 +74,28 @@ const RootQuery = new GraphQLObjectType({
         user: {
             type: UserType,
             args: {
-                id: {type: GraphQLID}
+                id: { type: GraphQLID }
             },
             resolve(parent, args) {
-                return _.find(usersData, {id: args.id});
+                return _.find(usersData, { id: args.id });
             }
         },
         hobby: {
             type: HobbyType,
             args: {
-                id: {type: GraphQLID}
+                id: { type: GraphQLID }
             },
             resolve(parent, args) {
-                return _.find(hobbiesData, {id: args.id});
+                return _.find(hobbiesData, { id: args.id });
+            }
+        },
+        post: {
+            type: PostType,
+            args: {
+                id: { type: GraphQLID }
+            },
+            resolve(parent, args) {
+                return _.find(postData, { id: args.id });
             }
         }
     }
